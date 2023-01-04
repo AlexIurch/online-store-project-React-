fbbd55927af97222f29f508821876d24bcc263a7
Alex Iurch <104131356+AlexIurch@users.noreply.github.com>
2023-01-04 12:57:52 +0100

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const data = {
     "first_name": "Oleksii",
     "last_name": "Iurchenko",
     "job_title": "Frontend developer",
-    "photo": "./IMG_3997.jpg",
+    "photo": "./Photo3.jpg",
     "phone": "+4741174573",
     "email": "zema.dak.ua@gmail.com",
     // "skype": "skype_123number",
