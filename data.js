99aa31f61f41c5a6fd31f6b60d0304f081ba1de7
Alex Iurch <104131356+AlexIurch@users.noreply.github.com>
2023-01-04 13:22:48 +0100

    Update data.js

--- data.js
+++ data.js
@@ -26,9 +26,12 @@ const data = {
     "education": [
         ["2002-2007 ", " National University of Life and Environmental Sciences of Ukraine "],
     ],
-    "achievements": [
+   "achievements": [
+        ["05.2022- ", " IT programming cours Kodehode Jobloop Haugesund "]',
         ["03.2021-07.2021 ", " HTML for JavaScript Developers "],
-        ["07.2021-to date ", " JavaScript v.2.0"],
+        ["07.2021-02.2022 ", " JavaScript v.2.0"],
+        ["02.2022-to date ", " Functions in JavaScript 2021"],
+        ["02.2022-05.2022 ", " JavaScript array methods"],
         ["08.2017-01.2020 ", " Enge skule,Norskspråk kurs"],
     ],
     "profile": "I am looking for new challenges and I think it will be interesting to work with you. I have a degree in mechanical engineering and is approved by NOKUT.To improve my skills, I am completing programming courses and looking for a new job where I can use my practical skills and become part of a great team. I also need a position where I can use my engineering and programming knowledge. It can also be an opportunity for me to develop both professionally and personally. I have diverse experiences. I worked with both administrative and manual tasks and enjoyed both. I consider myself as a positive person, I am structured and decision-oriented, I am ready to take responsibility. I learn quickly and gain new knowledge easily."
