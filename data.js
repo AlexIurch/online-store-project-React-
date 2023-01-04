3c7f24c9ac5e918922257da36276a778845f2b39
Alex Iurch <104131356+AlexIurch@users.noreply.github.com>
2023-01-04 13:16:08 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const data = {
     "first_name": "Oleksii",
     "last_name": "Iurchenko",
-    "job_title": "Frontend developerpppppp",
+    "job_title": "Frontend developer",
     "photo": "./Photo3.png",
     "phone": "+4741174573",
     "email": "zema.dak.ua@gmail.com",
@@ -32,5 +32,5 @@ const data = {
         ["07.2021-to date ", " JavaScript v.2.0"],
         ["08.2017-01.2020 ", " Enge skule,Norskspråk kurs"],
     ],
-    "profile": "I am looking for new challenges and I think it will be interesting to work with you. I have a degree in mechanical engineering and is approved by NOKUT.To improve my skills, I am completing programming courses and looking for a new job where I can use my practical skills and become part of a great team. I also need a position where I can use my engineering and programming knowledge. It can also be an opportunity for me to develop both professionally and personally. I have diverse experiences. My last job was as a farm manager and I had different types of work: maintenance of machines and various equipment, improvement of equipment and replacement of models for better equipment operation, animal care. I also have experience from Denmark and Ukraine. I worked with both administrative and manual tasks and enjoyed both. I consider myself as a positive person, I am structured and decision-oriented, I am ready to take responsibility. I learn quickly and gain new knowledge easily. I hope to have the opportunity to introduce myself in person."
+    "profile": "I am looking for new challenges and I think it will be interesting to work with you. I have a degree in mechanical engineering and is approved by NOKUT.To improve my skills, I am completing programming courses and looking for a new job where I can use my practical skills and become part of a great team. I also need a position where I can use my engineering and programming knowledge. It can also be an opportunity for me to develop both professionally and personally. I have diverse experiences. I worked with both administrative and manual tasks and enjoyed both. I consider myself as a positive person, I am structured and decision-oriented, I am ready to take responsibility. I learn quickly and gain new knowledge easily."
 };
