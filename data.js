e3a1b6a22f4117d5db492aa1aa269a2e6d528cab
Alex Iurch <104131356+AlexIurch@users.noreply.github.com>
2023-01-04 12:30:42 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,36 @@
+const data = {
+    "first_name": "Oleksii",
+    "last_name": "Iurchenko",
+    "job_title": "Frontend developer",
+    "photo": "images/IMG_3997.jpg",
+    "phone": "+4741174573",
+    "email": "zema.dak.ua@gmail.com",
+    // "skype": "skype_123number",
+    // "linkedin": "link_to_golinkedin",
+    "address": "Vikedal, Norway",
+    "skills": [
+        "Self-discipline and motivation for development and self-learning",
+        "Structured independently and has good collaboration skills",
+        "Results-oriented with good implementation skills",
+        // "Contact Negotiation",
+        // "Ad Buying & Planning",
+        "Product Positioning",
+        "Creative development",
+        "Search Engine Ads",
+        "Process Improvement",
+        "Basic knowledge HTML and CSS",
+        "Java Script",
+        "Adobe Creative Suite",
+        "Microsoft Office",
+        "Google Web Analytics",
+    ],
+    "education": [
+        ["2002-2007 ", " National University of Life and Environmental Sciences of Ukraine "],
+    ],
+    "achievements": [
+        ["03.2021-07.2021 ", " HTML for JavaScript Developers "],
+        ["07.2021-to date ", " JavaScript v.2.0"],
+        ["08.2017-01.2020 ", " Enge skule,Norskspråk kurs"],
+    ],
+    "profile": "I am looking for new challenges and I think it will be interesting to work with you. I have a degree in mechanical engineering and is approved by NOKUT.To improve my skills, I am completing programming courses and looking for a new job where I can use my practical skills and become part of a great team. I also need a position where I can use my engineering and programming knowledge. It can also be an opportunity for me to develop both professionally and personally. I have diverse experiences. My last job was as a farm manager and I had different types of work: maintenance of machines and various equipment, improvement of equipment and replacement of models for better equipment operation, animal care. I also have experience from Denmark and Ukraine. I worked with both administrative and manual tasks and enjoyed both. I consider myself as a positive person, I am structured and decision-oriented, I am ready to take responsibility. I learn quickly and gain new knowledge easily. I hope to have the opportunity to introduce myself in person."
+};
---
+++ experience.js
@@ -0,0 +1,42 @@
+const experience = [
+    {
+        "company": "Henning Guldhav jr",
+        "city": "Norway",
+        "from": "2016",
+        "to": '2020',
+        "position": "Farm manager",
+        "description": "Organization of the process of agricultural production and milk production. Maintenance of various systems and mechanisms"
+    },
+    {
+        "company": "Kulturgården Bjerkem ",
+        "city": "Norway",
+        "from": "2014",
+        "to": '2015',
+        "position": "Worker",
+        "description": "Production of organic agricultural products. Maintenance and repair of agricultural machinery and technical equipment."
+    },
+    {
+        "company": "Family business",
+        "city": "Ukraine",
+        "from": "2010",
+        "to": '2014',
+        "position": "Manager",
+        "description": "Work with construction sites. Installation of engineering systems."
+    },
+    {
+        "company": "Nørupgård ",
+        "city": "Denmark",
+        "from": "2008",
+        "to": '2010',
+        "position": "Trainee",
+        "description": "Production of agricultural products. Maintenance and repair of agricultural machinery and technical equipment"
+    },
+    {
+        "company": "Odessa Factory Agricultural Equipment",
+        "city": "Ukraine",
+        "from": "2007",
+        "to": '2008',
+        "position": "Sales Manager for Agricultural Equipment",
+        "description": "Sale of agricultural equipment"
+    }
+]
