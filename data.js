a6025bc24e33649e0fd4c10db73c364157308dcd
Alex Iurch <104131356+AlexIurch@users.noreply.github.com>
2023-01-04 13:16:52 +0100

    Update data.js

--- data.js
+++ data.js
@@ -5,8 +5,7 @@ const data = {
     "photo": "./Photo3.png",
     "phone": "+4741174573",
     "email": "zema.dak.ua@gmail.com",
-        "skype": "skype_123number",
-    "linkedin": "link_to_golinkedin",
+
     "address": "Vikedal, Norway",
     "skills": [
         "Self-discipline and motivation for development and self-learning",
